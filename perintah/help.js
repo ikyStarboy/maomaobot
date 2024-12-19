1ccd5ac0f6988b037c277db52824126358d8d0eb
Iky Starboy <154904513+ikyStarboy@users.noreply.github.com>
2024-12-19 13:43:31 +0700

    Create help.js

---
+++ perintah/help.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+async function Alya(api, event) {
+  const folderPath = path.join('./perintah');
+
+  try {
+    const files = await fs.readdir(folderPath);
+
+    const jsFiles = files.filter(file => path.extname(file) === '.js');
+
+    jsFiles.sort();
+
+    const commandList = jsFiles.map(file => path.basename(file, '.js')).join('\n');
+    api.sendMessage(`# Daftar perintah: \n\n${commandList}`, event.threadID, event.messageID);
+  } catch (error) {
+    
+  }
+}
+
+const config = { nama: "menu" };
+module.exports = { Alya, config };
