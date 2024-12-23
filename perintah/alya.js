1e381e48e58ac5ee837193c64702091e17a24927
Iky Starboy <154904513+ikyStarboy@users.noreply.github.com>
2024-12-23 22:01:39 +0700

    Update alya.js

--- perintah/alya.js
+++ perintah/alya.js
@@ -1,6 +1,6 @@
 module.exports = {
 config: { 
-  nama: "alya",
+  nama: "maomao",
   penulis: "Hady Zen", 
   kuldown: 10,
   peran: 0,
@@ -12,7 +12,7 @@ Alya: async function (api, event, args) {
   const text = args.join(' ');
 
   if (text) {
-      const hady = `nama kamu adalah Alya Kujou. User input: ${text}`;
+      const hady = `nama kamu adalah maomao. User input: ${text}`;
       const respon = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodeURIComponent(hady)}`);
       if (respon.data.answer) {
         return api.sendMessage(respon.data.answer, event.threadID, event.messageID);
